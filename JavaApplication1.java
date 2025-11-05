622de699d828fe03689ef932f508a098f10ff810
matteode-91 <matt3o26@gmail.com>
2025-11-05 19:34:26 +0000

    creazione attributi classe javaapplication1 e costruttore di default

--- JavaApplication1.java
+++ JavaApplication1.java
@@ -1,18 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package javaapplication1;
+import.java.util*;
 
-/**
- *
- * @author Serale
- */
 public class JavaApplication1 {
+    String app;
+    int versione;
 
-    /**
-     * @param args the command line arguments
-     */
+    public Applicazione{}
+
     public static void main(String[] args) {
         // TODO code application logic here
     }
