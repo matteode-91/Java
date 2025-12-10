ebe005bc392baf5782ca796a524cdce8dbac8583
matteode-91 <matt3o26@gmail.com>
2025-12-10 20:11:25 +0000

    Stampa su file

--- Automobile.java
+++ Automobile.java
@@ -1,18 +1,34 @@
-import java.util.*;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.Scanner;
+
 
 public class Automobile {
     String carburante;
     int potenza;
-    double serbatoio;
-    
+    int serbatoio;
+
     public Automobile(){}
+    
+    public Automobile(String carburante,int potenza,int serbatoio){
+        this.carburante=carburante;
+        this.potenza=potenza;
+        this.serbatoio=serbatoio;
+    }
     
     public void setcarburante(String carburante){this.carburante=carburante;}
     public void setpotenza(int potenza){this.potenza=potenza;}
-    public void setserbatoio(double serbatoio){this.serbatoio=serbatoio;}
+    public void setserbatoio(int serbatoio){this.serbatoio=serbatoio;}
     public String getcarburante(){return carburante;}
     public int getpotenza(){return potenza;}
     public double getserbatoio(){return serbatoio;}
+
+    public String fileCSV(){
+        return String.join(";",carburante,Integer.toString(potenza),Integer.toString(serbatoio));
+    }
    
 
     public boolean telecomando(boolean accendi){
@@ -39,10 +55,12 @@ public class Automobile {
     
 
     public static void main(String[] args) {
+    Path csv = Path.of("Automobile.csv");
+    Automobile Fiat=new Automobile("Gasolio",75,35);
     Automobile Ford=new Automobile();
     Ford.setcarburante("Benzina");
     Ford.setpotenza(125);
-    Ford.setserbatoio(50.45);
+    Ford.setserbatoio(50);
     Scanner sc=new Scanner(System.in);
     System.out.println("Digita true or false");
     boolean chiave=sc.nextBoolean();
